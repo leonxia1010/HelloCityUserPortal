a0371f59296e44efec446f253efa56eef58568f4
Paul Zhang <hzhang526@gmail.com>
2025-07-17 02:54:04 +1000

    feat(frontend): [SCRUM-44] Add example Banner component to better observe Navbar

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,9 +1,26 @@
 import React from 'react'
-
+import { Typography, Button } from '@mui/material';
 const Banner = () => {
   return (
-    <div>
-      Banner
+    <div className="relative w-[100vw] h-[57vh] bg-[url('/images/banner-image.jpeg')] bg-[length:100%] bg-center text-left">
+      <div className="absolute inset-0 bg-black/30" />
+      <div className="relative text-white pt-24 w-[70%] ml-[20vw]">
+        <Typography variant="h2" sx={{ marginTop: '10vh'}}>
+          Navigate your new city with  <br/>
+          <Typography variant="inherit" component="span" sx={{ color: 'primary.main' }}>
+            confidence
+          </Typography>
+        </Typography> 
+        <Typography variant="body1">
+          Get personalized guidance step by step checklists, and timeline <br/>
+          planning for setting into any city. Whether you're a tourist, student or <br/>
+          migrant
+        </Typography>
+        <Button variant='primary' sx={{marginTop: "6px"}}>
+          Try HelloCity
+        </Button>
+
+      </div>
     </div>
   )
 }
