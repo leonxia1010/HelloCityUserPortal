64f07e426a878312b7e14447a61ca133240cd178
Andy J <weiliangjia62@gmail.com>
2025-07-18 14:13:34 +0930

    [SCRUM-37] Modify button component, add clsx

--- src/app/components/button.tsx
+++ src/app/components/button.tsx
@@ -1,5 +1,6 @@
 "use client";
 import React, { FC } from "react"
+import clsx from "clsx";
 
 interface ButtonProps {
     variant: "primary" | "primary_register" | "secondary" | "tertiary";
@@ -11,28 +12,38 @@ interface ButtonProps {
 //this part will be removed once apllied MUI
 //----------------------
 const baseStyles = "rounded-xl px-6 py-3 font-semibold transition";
-const variantStyles: Record<ButtonProps["variant"], string> = {
-    primary:
-        "bg-primary-gradient text-white shadow-md hover:brightness-110 hover:shadow-lg",
-    primary_register:
-        "bg-modal-gradient text-white shadow-md hover:brightness-110 hover:shadow-lg",
-    secondary:
-        "bg-white text-primary border border-gray-300 shadow-sm hover:bg-[#EEF3FF] hover:border-[#A3B4FF]",
-    tertiary: "text-primary hover:underline",
-};
-const disabledStyles: Record<ButtonProps["variant"], string> = {
-    primary: "bg-disabled-gradient text-gray-400 shadow-none cursor-not-allowed opacity-70",
-    primary_register: "bg-disabled-gradient text-gray-400 shadow-none cursor-not-allowed opacity-70",
-    secondary: "bg-[#F3F3F3] text-[#A0A0A0] border border-[#DCDCDC] shadow-none cursor-not-allowed",
-    tertiary: "text-[#BBBBBB] cursor-not-allowed",
+const buttonStyles: Record<ButtonProps["variant"], { enabled: string; disabled: string }> = {
+    primary: {
+        enabled:
+            "bg-primary-gradient text-white shadow-md hover:brightness-110 hover:shadow-lg",
+        disabled:
+            "bg-disabled-gradient text-gray-400 shadow-none cursor-not-allowed opacity-70",
+    },
+    primary_register: {
+        enabled:
+            "bg-modal-gradient text-white shadow-md hover:brightness-110 hover:shadow-lg",
+        disabled:
+            "bg-disabled-gradient text-gray-400 shadow-none cursor-not-allowed opacity-70",
+    },
+    secondary: {
+        enabled:
+            "bg-white text-primary border border-gray-300 shadow-sm hover:bg-[#EEF3FF] hover:border-[#A3B4FF]",
+        disabled:
+            "bg-[#F3F3F3] text-[#A0A0A0] border border-[#DCDCDC] shadow-none cursor-not-allowed",
+    },
+    tertiary: {
+        enabled: "text-primary hover:underline",
+        disabled: "text-[#BBBBBB] cursor-not-allowed",
+    },
 };
 //----------------------
 
 const Button: FC<ButtonProps> = ({ variant, children, disabled, onClick }) => {
     //this part will be modified once applied MUI
-    const buttonClass = `
-        ${baseStyles}
-        ${disabled ? disabledStyles[variant] : variantStyles[variant]}`;
+    const buttonClass = clsx(
+        baseStyles,
+        disabled ? buttonStyles[variant].disabled : buttonStyles[variant].enabled
+    );
 
     return (
         <button className={buttonClass} disabled={disabled} onClick={onClick}>
