f48337472961977ce0e3b07c2fb510741d95af11
Jiujiu Hou <hlegetejojo@gmail.com>
2025-07-20 14:37:54 +1000

    fix(frontend): [SCRUM-42] resolve the merge conflict and change code style more clean, next will go to test

--- src/components/UserLabel.tsx
+++ src/components/UserLabel.tsx
@@ -1,11 +1,7 @@
-
 'use client';
 
 import React from 'react'
-import { useState , useEffect} from 'react';
 import Image from 'next/image';
-
-
 interface UserData {
   UserName?: string;
   PreferredName?: string;
@@ -13,36 +9,30 @@ interface UserData {
   LastJoinDate?: string;
 }
 
-
 const UserProfileCard: React.FC<UserData> = ({UserName, PreferredName, Avatar, LastJoinDate}) => {
-
-
-
-
+  
   return (
-        <div className="bg-gradient-to-r from-indigo-600 to-purple-600 p-6 text-white rounded-2xl shadow-xl hover:shadow-2xl flex items-center gap-2">
-              <div className="w-20 h-20 rounded-full bg-white flex items-center justify-center border-4 border-white ">
-                  {!Avatar ? (
-                    <span className="material-icons text-7xl text-gray-400">
-                      account_circle
-                    </span>
-                  ) : (
-                    <Image src={Avatar} alt="User Avatar" width={64} height={64} className="rounded-full" />
-                  )}
-              </div>     
-            <div className="">
-
-              <div className="text-xl font-bold">{UserName || "Unknown User"}</div>
-
-              <div className="text-indigo-200">@{PreferredName || "UnknownNickname"}</div>
-              <div className="text-sm mt-1 flex items-center gap-1">
-                <span className="material-icons text-indigo-200 ">
-                  access_time
-                </span>
-                last login: {LastJoinDate || "Unknown"}
-              </div>
-            </div>
-        </div> 
+    <div className="bg-gradient-to-r from-indigo-600 to-purple-600 p-6 text-white rounded-2xl shadow-xl hover:shadow-2xl flex items-center gap-2">
+      <div className="w-20 h-20 rounded-full bg-white flex items-center justify-center border-4 border-white ">
+        {!Avatar ? (
+          <span className="material-icons text-7xl text-gray-400">
+            account_circle
+          </span>
+          ) : (
+          <Image src={Avatar} alt="User Avatar" width={64} height={64} className="rounded-full" />
+        )}
+      </div>     
+      <div>
+        <span className="text-xl font-bold flex">{UserName || "Unknown User"}</span>
+        <span className="text-indigo-200 flex">@{PreferredName || "UnknownNickname"}</span>
+        <div className="text-sm mt-1 flex items-center gap-1">
+          <span className="material-icons text-indigo-200 text-base">
+            access_time
+          </span>
+            last login: {LastJoinDate || "Unknown"}
+        </div>
+      </div>
+    </div> 
   );
 };
 
