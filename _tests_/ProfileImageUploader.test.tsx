d9a10a613d1382f868c655e06d4f6124b0fc5ccc
zwh010 <harryzhong010@gmail.com>
2025-07-22 19:25:42 +1000

    ProfileImageUploader for review

---
+++ _tests_/ProfileImageUploader.test.tsx
@@ -0,0 +1,5 @@
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import ProfileImageUploader from '@/components/ProfileImageUploader'; 
+import '@testing-library/jest-dom';
+import React from 'react';
+
