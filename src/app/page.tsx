d572cf279ec74418345493f15db6d80fe021c4b8
taffy <ececanous@gmail.com>
2025-07-26 14:36:47 +1000

    First time push for scrum 45

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,25 @@
-import UserProfileCard from '../components/UserLabel';
-export default function Home() {
+'use client';
+
+import React from 'react';
+
+import { Banner, NavBar } from '@/components';
+
+import styles from './Homepage.module.scss';
+
+export default function HomePage() {
   return (
-    <div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
-      <h1>Hello World</h1>
+    <div className={styles['homepage-container']}>
+      <div className={styles['banner-background']} />
+
+      <div className={styles['blur-top']} >
+         <NavBar />
+      </div>
+  
+
+      <div className={styles['homepage-content']}>
+       
+        <Banner />
+      </div>
     </div>
   );
 }
--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,5 +1,7 @@
 "use client";
-import React, { FC } from "react"
+import type { FC } from "react";
+import React from "react"
+
 import clsx from "clsx";
 
 interface ButtonProps {
