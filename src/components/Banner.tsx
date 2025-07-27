6c7c22b0b26ed5f562c3f597d922064d8cd2a716
Paul Zhang <hzhang526@gmail.com>
2025-07-27 21:27:57 +1000

    fix(frontend): [SCRUM-65] Delete demo profile under banner page

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,32 +1,8 @@
 'use client'
-import React, { useEffect, useState } from 'react'
-import { Typography, Button, Box } from '@mui/material';
-import axios from 'axios'
-import { useTheme } from '@mui/material/styles';
+import React from 'react'
+import { Typography, Button } from '@mui/material';
 
 const Banner = () => {
-  const [userData, setUserData] = useState<any>(null);
-  const theme = useTheme();
-  const fetchUser = async(id: string | null) => {
-    if (!id) return;
-    try {
-      const response = await axios.get(`http://localhost:5000/api/user-profile/${id}`, {
-        headers: {
-          'Content-Type': 'application/json',
-          'Accept': '*/*'
-        }
-      });
-      setUserData(response.data);
-      console.log(response.data)
-    } catch (error) {
-      console.error('Failed to fetch user:', error);
-    }
-  }
-  useEffect(()=>{
-    const userId = localStorage.getItem('userId');
-    fetchUser(userId);
-  
-  }, [])
   return (
     <div className="relative w-screen h-[57vh] bg-[url('/images/banner-image.jpeg')] bg-cover bg-center bg-no-repeat text-left">
       <div className="absolute inset-0 bg-black/30" />
@@ -45,29 +21,6 @@ const Banner = () => {
         <Button variant='primary' sx={{marginTop: "6px"}}>
           Try HelloCity
         </Button>
-      </div>
-      <div className='absolute top-[110%] ml-[40%]'>
-        {userData &&
-          <Box p={3} 
-            color="primary"
-            sx={{background: theme.backgroundGradients.buttonPrimaryActive, borderRadius: '40px'}}
-          >
-            <Typography variant="h4" gutterBottom>
-              User Profile
-            </Typography>
-
-            <Typography variant='body1'><strong>Username:</strong> {userData.username}</Typography>
-            <Typography variant='body1'><strong>Email:</strong> {userData.email}</Typography>
-            <Typography variant='body1'><strong>Gender:</strong> {userData.gender}</Typography>
-            <Typography variant='body1'><strong>City:</strong> {userData.city || 'N/A'}</Typography>
-            <Typography variant='body1'><strong>Nationality:</strong> {userData.nationality || 'N/A'}</Typography>
-            <Typography variant='body1'><strong>Preferred Language:</strong> {userData.preferredLanguage}</Typography>
-            <Typography variant='body1'><strong>University:</strong> {userData.university || 'N/A'}</Typography>
-            <Typography variant='body1'><strong>Major:</strong> {userData.major || 'N/A'}</Typography>
-            <Typography variant='body1'><strong>Last Join Date:</strong> {new Date(userData.lastJoinDate).toLocaleString()}</Typography>
-            <Typography variant='body1'><strong>User ID:</strong> {userData.userId}</Typography>
-          </Box>
-        }
       </div>
     </div>
   )
