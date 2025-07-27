73eb09d38a55d01e0ec7976e4b38c4f44fd433b3
taffy <ececanous@gmail.com>
2025-07-28 03:45:40 +1000

    replaced the package-lock.json with the latest same file in main repoistory. Created a new samplepage to show SCRUM 45 homepage work, kept the old page.tsx as the same to resolve conflicts shown in github

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,13 @@
-'use client';
-
-import React from 'react';
-
-import { Banner, NavBar } from '@/components';
-
-import styles from './Homepage.module.scss';
-
-export default function HomePage() {
+import UserProfileCard from '../components/UserLabel';
+export default function Home() {
   return (
-    <div className={styles['homepage-container']}>
-      <div className={styles['banner-background']} />
-
-      <div className={styles['blur-top']} >
-         <NavBar />
-      </div>
-  
-
-      <div className={styles['homepage-content']}>
-       
-        <Banner />
-      </div>
+    <div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
+      <h1>Hello World</h1>
+      {/* Login link(must use <a> link to define Auth0 component) */}
+      <a href="/auth/login">Login</a>
+      {/* Logout link(must use <a> link to define Auth0 component) */}
+      <a href="/auth/logout" >Logout</a>
+      {/* conclusion: The Home component renders a simple layout with login and logout links. */}
     </div>
   );
 }
---
+++ src/app/samplepage.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+// I created this for resolve conflicts with the origin page.tsx.  This file is used for showing the visual effect of SCRUM-45 work.
+
+import React from 'react';
+
+import { Banner, NavBar } from '@/components';
+
+import styles from './Homepage.module.scss';
+
+export default function HomePage() {
+  return (
+    <div className={styles['homepage-container']}>
+      <div className={styles['banner-background']} />
+
+      <div className={styles['blur-top']} >
+         <NavBar />
+      </div>
+  
+
+      <div className={styles['homepage-content']}>
+       
+        <Banner />
+      </div>
+    </div>
+  );
+}
