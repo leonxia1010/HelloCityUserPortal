3612decea6298f5cbbcc90e62f01991c39d6d7ba
zwh010 <harryzhong010@gmail.com>
2025-07-28 16:12:11 +1000

    finish commit

--- _tests_/ProfileImageUploader.test.tsx
+++
@@ -1,5 +0,0 @@
-import { render, screen, fireEvent, waitFor } from '@testing-library/react';
-import ProfileImageUploader from '@/components/ProfileImageUploader'; 
-import '@testing-library/jest-dom';
-import React from 'react';
-
