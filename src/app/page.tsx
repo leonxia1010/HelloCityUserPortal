33380d90528dda6d6e7d3bd2d4dff548b5a16f18
Jiujiu Hou <hlegetejojo@gmail.com>
2025-07-31 00:38:11 +1000

    test(tools) : [SCRUM-69] fix the format of layout and page files by format:fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
-import Banner from '@/components/Banner';
-import NavBar from '@/components/NavBar';
+import Banner from "@/components/Banner";
+import NavBar from "@/components/NavBar";
 import UserProfileCard from '../components/UserLabel';
 
 export default function Home() {
   return (
-    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
+    <div>
+      <Banner />
       <h1>Hello World</h1>
       {/* Login link(must use <a> link to define Auth0 component) */}
       <a href="/auth/login">Login</a>
