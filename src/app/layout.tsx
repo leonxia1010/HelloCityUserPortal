3e9cdfcf11ac66633fd28d10f4af9e4acfa9a530
Jiujiu Hou <hlegetejojo@gmail.com>
2025-07-31 00:45:57 +1000

    test(tools) : [SCRUM-69] fix the format of layout and page files by format:fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,19 @@
-import type { Metadata } from "next";
-import "./globals.css";
+import type { Metadata } from 'next';
+import './globals.css';
 import { CssBaseline, ThemeProvider, StyledEngineProvider } from '@mui/material';
-import { Inter } from "next/font/google";
+import { Inter } from 'next/font/google';
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
 import websiteTheme from '@/theme/theme';
 import NavBar from '@/components/NavBar';
 import { LanguageProvider } from '@/contexts/LanguageContext';
 import { I18nProvider } from '@/contexts/I18nProvider';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "HelloCity - Landing Assistant for new cities",
+  title: 'HelloCity - Landing Assistant for new cities',
   description:
-    "HelloCity is an AI-powered landing assistant for international students, new immigrants, and travelers. It provides personalized checklists, timelines and document downloads to simplify visa processes, banking and housing and more — tackling fragmented information, language barriers, and complex procedures.",
+    'HelloCity is an AI-powered landing assistant for international students, new immigrants, and travelers. It provides personalized checklists, timelines and document downloads to simplify visa processes, banking and housing and more — tackling fragmented information, language barriers, and complex procedures.',
 };
 
 export default function RootLayout({
@@ -31,9 +31,7 @@ export default function RootLayout({
               <LanguageProvider>
                 <I18nProvider>
                   <NavBar />
-                  <div className='relative'>
-                    {children}
-                  </div>
+                  <div className="relative">{children}</div>
                 </I18nProvider>
               </LanguageProvider>
             </ThemeProvider>
