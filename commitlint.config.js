4279090453f27a456ddb849b6b8a029474fc138f
Jiujiu Hou <hlegetejojo@gmail.com>
2025-07-30 22:32:12 +1000

    test:wrong commit format

--- commitlint.config.js
+++ commitlint.config.js
@@ -3,12 +3,21 @@ module.exports = {
     'type-enum': [
       2,
       'always',
-      ['feat', 'fix', 'chore', 'tools', 'test', 'docs', 'refactor', 'style', 'perf', 'ci', 'build', 'revert'],
+      [
+        'feat',
+        'fix',
+        'chore',
+        'tools',
+        'test',
+        'docs',
+        'refactor',
+        'style',
+        'perf',
+        'ci',
+        'build',
+        'revert',
+      ],
     ],
-    'header-pattern': [
-      2,
-      'always',
-      /^[a-z]+\([a-z]+\) : \[SCRUM-\d+\] .+$/,
-    ],
+    'header-pattern': [2, 'always', /^[a-z]+\([a-z]+\) : \[SCRUM-\d+\] .+$/],
   },
 };
