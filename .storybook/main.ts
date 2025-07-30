efa682f25a34380c349767841a7ac255422e647b
Pzhang768 <122713177+Pzhang768@users.noreply.github.com>
2025-07-30 20:56:54 +1000

    Revert "Scrum 68 fix storybook package conflict between webpack and vite (#17)"
    This reverts commit c27ae61c82a18d2adf15bfd3ea1c5c6a8295d3ce.

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -6,14 +6,17 @@ const config: StorybookConfig = {
     "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
   ],
   "addons": [
+    "@chromatic-com/storybook",
     "@storybook/addon-docs",
     "@storybook/addon-a11y",
+    "@storybook/addon-vitest"
   ],
   "framework": {
     "name": "@storybook/nextjs-vite",
     "options": {}
   },
-  staticDirs: ["../public"]
-
+  "staticDirs": [
+    "..\\public"
+  ]
 };
 export default config;
