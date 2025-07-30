9b4e595479b91e2f847563bc05e9f4d254949a98
Jiujiu Hou <hlegetejojo@gmail.com>
2025-07-30 23:59:46 +1000

    test(tools) : [SCRUM-69] format:fix change the files then recommit

--- __tests__/utils/TestWrapper.tsx
+++ __tests__/utils/TestWrapper.tsx
@@ -4,9 +4,7 @@ import { I18nProvider } from '@/contexts/I18nProvider';
 
 // This is a wrapper component that provides language context for testing purposes
 export const I18nTestWrapper: React.FC<{ children: React.ReactNode }> = ({ children }) => (
-    <LanguageProvider>
-    <I18nProvider>
-    { children }
-    </I18nProvider>
-    </LanguageProvider>
+  <LanguageProvider>
+    <I18nProvider>{children}</I18nProvider>
+  </LanguageProvider>
 );
--- jest.config.ts
+++ jest.config.ts
@@ -34,9 +34,7 @@ const config: InitialOptionsTsJest = {
   coverageDirectory: 'coverage',
 
   // An array of regexp pattern strings used to skip coverage collection
-  testPathIgnorePatterns: [
-    '<rootDir>/__tests__/utils/',
-  ],
+  testPathIgnorePatterns: ['<rootDir>/__tests__/utils/'],
   // coveragePathIgnorePatterns: [
   //   "/node_modules/"
   // ],
